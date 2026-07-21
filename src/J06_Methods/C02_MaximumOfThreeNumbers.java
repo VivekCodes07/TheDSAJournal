4c27fe5b463093e8ac2fec03009afeafb68574c5
Vivek <vivekkrdas03@gmail.com>
2026-07-21 11:11:20 +0530

    Add program to find maximum of three numbers using Math.max()

---
+++ src/J06_Methods/C02_MaximumOfThreeNumbers.java
@@ -0,0 +1,25 @@
+package J06_Methods;
+
+import java.util.Scanner;
+
+public class C02_MaximumOfThreeNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        int max = Math.max(a, Math.max(b, c));
+
+        System.out.println("Maximum number is: " + max);
+
+        sc.close();
+    }
+}
