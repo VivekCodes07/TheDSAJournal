d319a2d4246cad191dec15f4957a21edf952fd07
Vivek <vivekkrdas03@gmail.com>
2026-07-21 11:06:06 +0530

    Add examples of Java built-in Math methods

---
+++ src/J06_Methods/C01_BuiltInMethods.java
@@ -0,0 +1,60 @@
+package J06_Methods;
+
+public class C01_BuiltInMethods {
+    public static void main(String args[]) {
+
+        // Square Root
+        System.out.println("Math.sqrt(10) = " + Math.sqrt(10));
+
+        // Cube Root
+        System.out.println("Math.cbrt(10) = " + Math.cbrt(10));
+
+        // Value of PI
+        System.out.println("Math.PI = " + Math.PI);
+
+        // Euler's Number
+        System.out.println("Math.E = " + Math.E);
+
+        // Floor
+        System.out.println("Math.floor(6.8) = " + Math.floor(6.8));
+
+        // Ceiling
+        System.out.println("Math.ceil(6.2) = " + Math.ceil(6.2));
+
+        // Round
+        System.out.println("Math.round(6.5) = " + Math.round(6.5));
+
+        // Absolute Value
+        System.out.println("Math.abs(-15) = " + Math.abs(-15));
+
+        // Maximum
+        System.out.println("Math.max(25, 40) = " + Math.max(25, 40));
+
+        // Minimum
+        System.out.println("Math.min(25, 40) = " + Math.min(25, 40));
+
+        // Power
+        System.out.println("Math.pow(2, 5) = " + Math.pow(2, 5));
+
+        // Random Number
+        System.out.println("Math.random() = " + Math.random());
+
+        // Sine
+        System.out.println("Math.sin(Math.PI / 2) = " + Math.sin(Math.PI / 2));
+
+        // Cosine
+        System.out.println("Math.cos(0) = " + Math.cos(0));
+
+        // Tangent
+        System.out.println("Math.tan(Math.PI / 4) = " + Math.tan(Math.PI / 4));
+
+        // Logarithm (Natural Log)
+        System.out.println("Math.log(10) = " + Math.log(10));
+
+        // Base-10 Logarithm
+        System.out.println("Math.log10(100) = " + Math.log10(100));
+
+        // Exponential
+        System.out.println("Math.exp(2) = " + Math.exp(2));
+    }
+}
